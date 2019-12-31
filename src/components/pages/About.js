a3b45f17fa883344c24228e9710862e6adb74a26
Graham Vasquez <rescus1221@gmail.com>
2019-12-31 12:52:09 -0500

    Basic React Todo List from tutorial video

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is a Todo List app v1.0.0. It is a test app for learning React.</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
